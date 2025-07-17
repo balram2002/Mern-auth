780e91c0a169184a7c57773f97332670641d3543
Balram Dhakad <bdhakad886@gmail.com>
2025-07-17 17:58:06 +0530

    Version 1.3

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ import { connectDB } from "./db/connectDB.js";
 import authRoutes from "./routes/auth.route.js";
 
 dotenv.config();
-
+y
 const app = express();
 const PORT = process.env.PORT || 5000;
 // const __dirname = path.resolve();
@@ -21,6 +21,10 @@ app.use(cors({
 }));
 app.use(express.json());
 app.use(cookieParser());
+
+app.get('/', (req, res) => {
+    res.send('authix api running new deploy');
+});
 
 app.use("/api/auth", authRoutes);
 
