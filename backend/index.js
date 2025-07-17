72cf8aab5d78bbc6041ce648347f3467f8259fda
Balram Dhakad <bdhakad886@gmail.com>
2025-07-17 17:39:16 +0530

    Version 1.0

--- backend/index.js
+++ backend/index.js
@@ -12,20 +12,23 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-const __dirname = path.resolve();
+// const __dirname = path.resolve();
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
-
+app.use(cors({
+    origin: ["http://localhost:5173", "http://localhost:5000"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+}));
 app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 
-app.use(express.static(path.join(__dirname, "/frontend/dist")));
+// app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-app.get("*", (req, res) => {
-	res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-});
+// app.get("*", (req, res) => {
+// 	res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+// });
 
 app.listen(PORT, () => {
 	connectDB();
