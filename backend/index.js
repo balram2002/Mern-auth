e76e17d7ee46843d6c389f77732e65b3e78ffc6f
Balram Dhakad <bdhakad886@gmail.com>
2025-07-17 18:16:21 +0530

    Version 1.5

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 5000;
 // const __dirname = path.resolve();
 
 app.use(cors({
-    origin: ["http://localhost:5173", "http://localhost:5000"],
+    origin: ["https://authix-bd.vercel.app", "https://authix-api.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
 }));
